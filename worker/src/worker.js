e948d722e93e8734bac8038a45fa39dae898347c
aayush patel <intern.ap2@nuvo.ai>
2026-08-29 17:40:32 +0530

    second commit

--- worker/src/worker.js
+++ worker/src/worker.js
@@ -1,3 +1,37 @@
-const { Worker } = require('bullmq');
+const { Worker } = require("bullmq");
 require("dotenv").config();
 
+const connection = {
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+};
+
+const worker = new Worker(
+  "csv-processing",
+  async (job) => {
+    console.log("Job received!");
+    console.log("Job ID:", job.id);
+    console.log("Job name:", job.name);
+    console.log("Job data:", job.data);
+
+    // Later we will process CSV here
+
+    return {
+      success: true,
+    };
+  },
+  {
+    connection,
+  }
+);
+
+worker.on("completed", (job) => {
+  console.log(`Job ${job.id} completed`);
+});
+
+worker.on("failed", (job, error) => {
+  console.log(`Job ${job.id} failed`);
+  console.error(error);
+});
+
+console.log("Worker is running...");
